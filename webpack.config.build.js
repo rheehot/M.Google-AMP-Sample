e057527496f328dff9ae93bd6310184717fd384a
sungmin-ma <masung1030@gmail.com>
2019-04-18 17:46:29 +0900

    amp first

---
+++ webpack.config.build.js
@@ -0,0 +1,21 @@
+ const merge = require('webpack-merge');
+ const baseConfig = require('./webpack.config');
+ const CleanWebpackPlugin = require('clean-webpack-plugin');
+ const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+ const webpackBuildConfig = merge(baseConfig, {
+    //  output: {
+    //      path: __dirname + "/docs",
+    //      //  publicPath: "/Note.NET"
+    //  },
+    //  plugins: [
+    //      new CleanWebpackPlugin({
+    //          verbose: true
+    //      }),
+    //      new CopyWebpackPlugin([
+    //         { from: './src/CNAME' }
+    //     ])
+    //  ]
+ });
+ module.exports = webpackBuildConfig
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const webpackBasicConfig = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        // path: __dirname + "/docs",
+        // publicPath: "/Note.NET",
+        chunkFilename: './js/[name].bundle.js',
+    },
+    module: {
+        rules: [
+        //     {
+        //     test: /\.vue$/,
+        //     loader: "vue-loader"
+        // }, 
+        {
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /tus-js-client|node_modules\/(?!(dom7|ssr-window|vue-plugin-load-script)\/).*/
+        }, 
+        // {
+        //     test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //     exclude: /node_modules/,
+        //     loader: 'file-loader?name=img/[path][name].[ext]&context=./app/images'
+        // }, {
+        //     test: /\.css$/,
+        //     use: ['style-loader', 'css-loader'],
+        // }, 
+    ]
+    },
+    resolve: {
+        // alias: {
+        //     "vue$": "vue/dist/vue.esm.js",
+        //     'View': path.resolve(__dirname, './src/Vue'),           
+        //     'VueConfig': path.resolve(__dirname, './src/Vue/config'),
+        // },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [
+        // new VueLoaderPlugin(),
+        new HtmlWebPackPlugin({
+            // favicon: './src/assets/favicon.ico',
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        namedModules: true,
+        noEmitOnErrors: true,
+        concatenateModules: true,
+        mangleWasmImports: true,
+        removeAvailableModules: true,
+        removeEmptyChunks: true,
+        mergeDuplicateChunks: true,
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: 1,
+            minSize: 0,
+            // maxSize: 200,
+        },
+    }
+}
+module.exports = webpackBasicConfig;
